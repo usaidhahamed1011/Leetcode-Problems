91dcf811d5dd0eb79f1010fc118b2305f628f987
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-11 11:17:14 +0530

    [121] [Time Beats: 100.00%] [Memory Beats: 24.82%] - LeetPush

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyprice = prices[0];
+        int profit = 0;
+        for(int i=0;i<prices.size();i++){
+            if(buyprice>prices[i]){
+                buyprice = prices[i];
+            }
+            profit = max(profit , prices[i]-buyprice);
+        }
+        return profit;
+    }
+};
