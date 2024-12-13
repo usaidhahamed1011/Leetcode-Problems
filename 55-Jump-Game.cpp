75f7c07988bc092d96380ed1e1cebaf18e5f2a78
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-13 11:09:24 +0530

    [55] [Time Beats: 100.00%] [Memory Beats: 17.53%] - LeetPush

--- 55-Jump-Game.cpp
+++ 55-Jump-Game.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size()-1;
-        for(int i=nums.size()-1;i>=0;i--){
+        int n=nums.size()-1;
+        for(int i=n-1;i>=0;i--){
             int d = i+nums[i];
             if(d>=n){
                 n=i;
