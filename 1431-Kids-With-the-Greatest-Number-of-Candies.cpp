cce856112d7186b16d497f70213c5011262826ee
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-16 21:52:13 +0530

    [1431] [Time Beats: 100.00%] [Memory Beats: 7.50%] - LeetPush

--- 1431-Kids-With-the-Greatest-Number-of-Candies.cpp
+++ 1431-Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         int n = candies.size();
-        int maxi = *max_element(candies.begin(),candies.end());
+        int maxi = 0;
+        for(int i=0;i<n;i++){
+            maxi = max(maxi,candies[i]);
+        }
         vector<bool> result(n);
-         n=0;
-        for(int i : candies){
-            if(i+extraCandies >= maxi){
-                result[n++] = true;
+        for(int i=0;i<n;i++){
+            if(candies[i]+extraCandies >= maxi){
+                result[i] = true;
             }
             else{
-                result[n++] = false;
+                result[i] = false;
             }
         }
         return result;
