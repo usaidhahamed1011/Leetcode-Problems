8dec5dea738bbab2d4161c7de4b93260de865871
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-16 20:26:53 +0530

    [9] [Time Beats: 100.00%] [Memory Beats: 49.18%] - LeetPush

--- 9-Palindrome-Number.cpp
+++ 9-Palindrome-Number.cpp
@@ -4,15 +4,14 @@ public:
         if(x<0){
             return false;
         }
-        long long  temp;
-        temp = x;
-        int rem;
+        int rem = 0;
+        long long temp = x;
         long long reverse = 0;
-        while(temp!=0){
-            rem = temp %10;
-            reverse = reverse *10 + rem;
-            temp /=10;
+        while(temp>0){
+            rem = temp%10;
+            reverse = reverse*10+rem;
+            temp/=10;
         }
-        return ( reverse==x);
+        return reverse == x;
     }
 };
