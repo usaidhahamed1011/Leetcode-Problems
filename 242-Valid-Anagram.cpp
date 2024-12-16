67fde5dc84dd23eb6c83cb4040dcb96e3906ad43
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-16 11:07:40 +0530

    [242] [Time Beats: 33.91%] [Memory Beats: 57.29%] - LeetPush

---
+++ 242-Valid-Anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.length()==t.length()){
+            for(int i=0;i<s.length();i++){
+                if(s[i] == t[i]){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(s.length()!=t.length()){
+            return false;
+        }
+        return true;
+    }
+};
