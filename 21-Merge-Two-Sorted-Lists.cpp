d95106af5d85e05feabcdbca805908658c4496a6
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-27 10:01:56 +0530

    [21] [Time Beats: 4.30%] [Memory Beats: 12.67%] - LeetPush

--- 21-Merge-Two-Sorted-Lists.cpp
+++ 21-Merge-Two-Sorted-Lists.cpp
@@ -11,16 +11,35 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-        if(!list2) return list1;
-        if(list1->val <= list2->val){
-            list1->next = mergeTwoLists(list1->next , list2);
-            return list1;
+        // if(!list1) return list2;
+        // if(!list2) return list1;
+        // if(list1->val <= list2->val){
+        //     list1->next = mergeTwoLists(list1->next , list2);
+        //     return list1;
+        // }
+        // else{
+        //     list2->next = mergeTwoLists(list1 , list2->next);
+        //     return list2;
+        // }
+        vector<int> num;
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
+        while(t1!=NULL){
+            num.push_back(t1->val);
+            t1=t1->next;
         }
-        else{
-            list2->next = mergeTwoLists(list1 , list2->next);
-            return list2;
+        while(t2!=NULL){
+            num.push_back(t2->val);
+            t2=t2->next;
         }
+        sort(num.begin(),num.end());
+        ListNode* temp = new ListNode;
+        ListNode* dumm = temp;
+        for(int i=0;i<num.size();i++){
+            dumm->next =new ListNode(num[i]);
+            dumm = dumm->next;
+        }
+        return temp->next;
         
     }
 };
