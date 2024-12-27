fc0d6006317d941aebbe0deca8f78c4113f9a640
usaidhahamed1011 <149071116+usaidhahamed1011@users.noreply.github.com>
2024-12-27 09:23:02 +0530

    [141] [Time Beats: 7.98%] [Memory Beats: 11.44%] - LeetPush

---
+++ 141-Linked-List-Cycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode *> s;
+        while(head!=NULL){
+            if(s.contains(head)){
+                return true;
+            }
+            s.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
