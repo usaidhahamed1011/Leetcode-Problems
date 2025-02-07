44334ba5498c62249b44aef0bc12fbabeb67cdcb
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-07 13:28:32 +0530

    Time: 5 ms (24.71%), Space: 8.6 MB (24.41%) - LeetHub

--- 3469-maximum-height-of-a-triangle/3469-maximum-height-of-a-triangle.cpp
+++ 3469-maximum-height-of-a-triangle/3469-maximum-height-of-a-triangle.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int maxHeightOfTriangle(int red, int blue) {
-        return max(fun(red,blue),fun(blue,red));
-    }
-private:
-    int fun(int red ,int blue){
         int h = 0;
         int i = 1;
+        int r = red;
+        int b = blue;
         while (true) {
             if (i % 2 == 1) {
                 if (red >= i) {
@@ -24,6 +22,26 @@ private:
             h++;
             i++;
         }
-        return h;
+        int h1 = 0;
+        int i1 = 1;
+        while (true) {
+            if (i1 % 2 == 0) {
+                if (r >= i1) {
+                    r -= i1;
+                } else {
+                    break;
+                }
+            } else {
+                if (b >= i1) {
+                    b -= i1;
+                } else {
+                    break;
+                }
+            }
+            h1++;
+            i1++;
+        }
+        return max(h,h1);
+
     }
 };
