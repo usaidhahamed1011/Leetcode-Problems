925b7a9836d9fdcc87d9291c34d9a79380343149
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-15 14:18:25 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.65%) - LeetHub

---
+++ 3625-stone-removal-game/3625-stone-removal-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAliceWin(int n) {
+        int c = 0;
+        int  p = 10;
+        while(n>=p){
+            n=n-p;
+            c++;
+            p--;
+        }
+        return c%2!=0;
+    }   
+};
