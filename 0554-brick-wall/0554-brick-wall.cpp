de7ce54ffbfa0fb867c00d116c2fb10c689e8e47
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-18 13:46:07 +0530

    Time: 14 ms (24.62%), Space: 28.3 MB (8.96%) - LeetHub

---
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int leastBricks(vector<vector<int>>& wall) {
+        map<int, int> mp;
+        for(auto w: wall) {
+            long long sum=0;
+            for(int x: w) {
+                sum += x;
+                mp[sum]++;
+            }
+        }
+        int m1{INT_MIN}, m2{INT_MIN};
+        for(auto x: mp) {
+            if(x.second > m1) {
+                m2 = m1;
+                m1 = x.second;
+            } else if(x.second > m2) {
+                m2 = x.second;
+            }
+        }
+        return m2 == INT_MIN ? wall.size() : wall.size() - m2;
+    }
+};
