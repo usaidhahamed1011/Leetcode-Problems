2b09513b77c431442f3b1e56b7248cd59a787091
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-18 09:38:12 +0530

    [2375] [Time Beats: 100.00%] [Memory Beats: 18.66%] - LeetPush

---
+++ 2375-Construct-Smallest-Number-From-DI-String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        string ans="";
+        stack<int> st;
+        for(int i=0;i<=pattern.size();i++){
+            st.push(i+1);
+            if(pattern[i]=='I'|| i==pattern.size()){
+            while(!st.empty()){
+                ans+=to_string(st.top());
+                st.pop();
+            }
+            }
+        }
+        return ans;
+    }
+};
