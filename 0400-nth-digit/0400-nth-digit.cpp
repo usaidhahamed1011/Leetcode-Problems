7a94eaf249b8c9d0af78491f5432bbe37cac445b
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-20 13:21:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.86%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.cpp
+++ 0400-nth-digit/0400-nth-digit.cpp
@@ -1,19 +1,18 @@
 class Solution {
- public:
-  int findNthDigit(int n) {
-    int digitSize = 1;
-    int startNum = 1;
-    long count = 9;
-
-    while (digitSize * count < n) {
-      n -= digitSize * count;
-      ++digitSize;
-      startNum *= 10;
-      count *= 10;
+public:
+    int findNthDigit(int n) {
+        long long digitsize = 1;
+        int start = 1;
+        int count = 9;
+        while (digitsize * count < n) {
+            n -= (digitsize * count);
+            ++digitsize;
+            start *= 10;
+            count *= 10;
+        }
+        start += (n - 1) / digitsize;
+        int index = (n - 1) % digitsize;
+        string s = to_string(start);
+        return s[index] - '0';
     }
-
-    const int targetNum = startNum + (n - 1) / digitSize;
-    const int index = (n - 1) % digitSize;
-    return to_string(targetNum)[index] - '0';
-  }
 };
