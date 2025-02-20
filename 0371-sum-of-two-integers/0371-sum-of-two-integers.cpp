74cccfbfae9a2fda92c1823abd0470bb57382669
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-20 14:51:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.52%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while(b!=0){
-            int c = (a&b)<<1;
+            int c = (a&b);
             a=a^b;
-            b = c;
+            b = c<<1;
         }
         return a;
     }
