d85e900eee6c6ccf22e26b83c882fe96f5fb6799
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-24 13:17:37 +0530

    Time: 34 ms (9.09%), Space: 11.2 MB (27.27%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.length() > 2) {
+            string new_s = "";
+            for (size_t i = 0; i < s.length() - 1; ++i) {
+                int new_digit = (s[i] - '0' + s[i + 1] - '0') % 10;
+                new_s += to_string(new_digit);
+            }
+            s = new_s;
+        }
+        return s[0] == s[1];
+    }
+};
